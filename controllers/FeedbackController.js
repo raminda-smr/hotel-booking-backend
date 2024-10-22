eda80991301a9cb96129807564d727578f786c77
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-10-22 13:35:00 +0530

    created createFeedback function

--- controllers/FeedbackController.js
+++ controllers/FeedbackController.js
@@ -1,4 +1,39 @@
+import { authenticateCustomer } from "../helpers/Authenticate.js";
+import Feedback from "../models/Feedback.js";
+
 export function  createFeedback(req,res){
+
+    authenticateCustomer(req,res, "You must login as a customer to create a feedback!")
+
+    const feedback = req.body
+
+    const newFeedback = new Feedback(feedback)
+
+    newFeedback.save().then(
+        (result) => {
+            if(result){
+                res.json({
+                    message:"Feedback created",
+                    result: result
+                })
+            }
+            else{
+                res.status(500).json({
+                    message:"Feedback creation failed",
+                })
+            }
+        }
+    ).catch(
+        (err) => {
+            if(err != undefined){
+                res.status(500).json({
+                    message:"Feedback creation failed",
+                    error: err
+                })
+            }
+        }
+    )
+
 
 }
 
