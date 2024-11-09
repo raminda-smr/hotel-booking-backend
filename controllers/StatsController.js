621164bd9a08a85fe4cc78b66f5af8438c4d7e5c
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-11-09 19:16:23 +0530

    completed get dashboard booking data function

--- controllers/StatsController.js
+++ controllers/StatsController.js
@@ -6,6 +6,7 @@ import Feedback from "../models/Feedback.js"
 import GalleryItem from "../models/GalleryItem.js"
 import { authenticateAdmin } from "../helpers/Authenticate.js"
 
+
 export function getModuleStats(req, res) {
 
     const getModuleData = async () => {
@@ -47,19 +48,71 @@ export function getDashboardBookingStats(req, res) {
     // authenticateAdmin(req, res, "You must login as a admin to get dashboard data!")
 
     const getDashboardBookingData = async () => {
-        const today = new Date();
 
-        const tomorrow = new Date(localToday);
-        tomorrow.setDate(localToday.getDate() + 1);
+        let today = new Date()
 
-        console.log("Today:", today);
-        console.log("Tomorrow:", tomorrow);
+        // remove time offset  
+        today = new Date(today.getTime() - (process.env.TIME_OFFSET * 60 * 1000))
+
+        let tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+
+        let dayAfterTomorrow = new Date(today);
+        dayAfterTomorrow.setDate(today.getDate() + 2);
+
+        // get date strings
+        today = today.toISOString().split('T')[0]
+        tomorrow = tomorrow.toISOString().split('T')[0]
+        dayAfterTomorrow = dayAfterTomorrow.toISOString().split('T')[0]
 
         try {
-            
+
+            const todaysBookingsCount = await Booking.countDocuments({
+                start: { $gt: new Date(today) },
+                end: { $lt: new Date(tomorrow) },
+                status: { $nin: ["canceled", "deleted", "refunded"] }
+            })
+
+            const tomorrowsBookingsCount = await Booking.countDocuments({
+                start: { $gt: new Date(tomorrow) },
+                end: { $lt: new Date(dayAfterTomorrow) },
+                status: { $nin: ["canceled", "deleted", "refunded"] }
+            })
+
+            const upcomingBookingsCount = await Booking.countDocuments({
+                start: { $gt: new Date(today) }
+            })
+
+            const completedBookingsCount = await Booking.countDocuments({
+                status: "completed"
+            })
+
+            const totaldBookingsCount = await Booking.countDocuments()
+
+            const todaysBookings = await Booking.find({
+                start: { $gt: new Date(today) },
+                end: { $lt: new Date(tomorrow) },
+                status: { $nin: ["canceled", "deleted", "refunded"] }
+            }).limit(2)
+
+            const tomorrowsBookings = await Booking.find({
+                start: { $gt: new Date(tomorrow) },
+                end: { $lt: new Date(dayAfterTomorrow) },
+                status: { $nin: ["canceled", "deleted", "refunded"] }
+            }).limit(2)
+
 
             return {
-               
+                today: today,
+                tomorrow: tomorrow,
+                dayAfterTomorrow: dayAfterTomorrow,
+                todaysBookingsCount: todaysBookingsCount,
+                tomorrowsBookingsCount: tomorrowsBookingsCount,
+                upcomingBookingsCount: upcomingBookingsCount,
+                completedBookingsCount: completedBookingsCount,
+                totaldBookingsCount: totaldBookingsCount,
+                todaysBookings: todaysBookings,
+                tomorrowsBookings: tomorrowsBookings
             }
         } catch (e) {
             console.error("Error getting count: ", e)
