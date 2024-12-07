f9b6052a9d6e0397ecc8472acce6a6bd7c3a0b5e
raminda-smr <38224388+raminda-smr@users.noreply.github.com>
2024-12-07 18:23:25 +0530

    create customer booking query functions

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,13 +1,16 @@
 import express from "express"
-import { createBookings, deleteBooking, getBookings, updateBooking, getBookingById, getAvailableRooms } from "../controllers/BookingController.js"
+import { createBookings, deleteBooking, getBookings, updateBooking, getBookingById, getAvailableRooms, getCustomerBookings, getCustomerBookingById } from "../controllers/BookingController.js"
 let bookingRoutes = express.Router()
-
-
-bookingRoutes.post("/", createBookings)
 
 bookingRoutes.get("/", getBookings)
 
+bookingRoutes.post("/", createBookings)
+
 bookingRoutes.post("/get-available-rooms", getAvailableRooms)
+
+bookingRoutes.get("/customer/byId/:bookingId", getCustomerBookingById)
+
+bookingRoutes.get("/customer/:type", getCustomerBookings)
 
 bookingRoutes.get("/:bookingId", getBookingById)
 
